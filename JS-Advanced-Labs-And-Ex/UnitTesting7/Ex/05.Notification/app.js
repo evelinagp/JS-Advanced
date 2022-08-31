980c894e4d393e55a102e01a9ff311ed0039075c
Evelina Pancheva <81183303+evitapk@users.noreply.github.com>
2022-08-31 23:02:35 +0300

    Delete JS-Advanced-Labs-And-Ex/UnitTesting7 directory

--- JS-Advanced-Labs-And-Ex/UnitTesting7/Ex/05.Notification/app.js
+++
@@ -1,9 +0,0 @@
-function notify(message) {
-    const resultDiv = document.querySelector("#notification");
-    resultDiv.style.display = "block";
-    resultDiv.textContent = message;
-
-    resultDiv.addEventListener('click', () => {
-        resultDiv.style.display = "none";
-    });
-}
